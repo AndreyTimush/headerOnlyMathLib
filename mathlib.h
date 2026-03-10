0764d7c270e4cdc3183fb366f8c67ebd150fcc7a
Andrey Timush <andrikshumit@gmail.com>
2026-03-10 07:40:31 +0000

    add all functions to mathlib

--- mathlib.h
+++ mathlib.h
@@ -1,8 +1,34 @@
 #pragma once
 namespace mathlib
 {
-	int sum(int a, int b) 
-	{
-		return a+b;
+	int sum(int a, int b) {
+		return a + b;
+	}
+
+	int subtraction(int a, int b) {
+		return a - b;
+	}
+	
+	float division(int a, int b) 
+	{ 
+		if (b != 0) {
+			return a / b;
+		}
+	}
+
+	int multiply(int a, int b) {
+		return a * b;
+	}
+
+	int pow(int a, int b) {
+		int res = 1;
+		for (int i = 1; i <= b; i++) {
+			res = res * a;
+		}
+		return res;
+	}
+
+	int factorial(int a) {
+		return a * factorial(a - 1);
 	}
 }
