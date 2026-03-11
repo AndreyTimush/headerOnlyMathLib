9537c0fec89dd22bcf67754f96af69fb1c771667
Andrey Timush <andrikshumit@gmail.com>
2026-03-11 10:54:51 +0000

    add check overflow

--- mathlib.h
+++ mathlib.h
@@ -2,8 +2,8 @@
 namespace mathlib
 {
 	int sum(int a, int b) {
-		long long summa = a + b;
-		if (a + b > INT_MAX) {
+		long long summa = static_cast<long long>(a) + static_cast<long long>(b);
+		if (summa > INT_MAX || summa < INT_MIN) {
 			printf("Ошибка переполнения");
 			return 0;
 		}
@@ -11,31 +11,49 @@ namespace mathlib
 	}
 
 	int subtraction(int a, int b) {
+		long long res = static_cast<long long>(a) - static_cast<long long>(b);
+		if (res > INT_MAX || res < INT_MIN) {
+			printf("Ошибка переполнения");
+			return 0;
+		}
 		return a - b;
 	}
 	
 	float division(int a, int b) 
 	{ 
 		if (b != 0) {
-			return a / b;
+			return static_cast<float>(a) / static_cast<float>(b);
 		}
 		return -1;
 	}
 
 	int multiply(int a, int b) {
+		long long mul = static_cast<long long>(a) * static_cast<long long>(b);
+		if (mul > INT_MAX || mul < INT_MIN) {
+			printf("Ошибка переполнения");
+			return 0;
+		}
 		return a * b;
 	}
 
 	int pow(int a, int b) {
-		int res = 1;
+		long long res = 1;
 		for (int i = 1; i <= b; i++) {
 			res = res * a;
+			if (res > INT_MAX || res < INT_MIN) {
+				printf("Ошибка переполнения");
+				return 0;
+			}
 		}
 		return res;
 	}
 
 	int factorial(int a) {
 		int result = 1;
+		if (a > 12) {
+			printf("Ошибка переполнения");
+			return 0;
+		}
 		if (a <= 1) {
 			return result;
 		}
